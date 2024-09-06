4470dc506c8598c17fc79919153051cb51be4e94
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-06 16:26:05 +0700

    feat(root): add import clerk

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,21 @@
+import { Header, Editor } from "@/components";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Document() {
-  return <div>documents</div>;
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="documents-title">This is a fake dokumens</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
+    </div>
+  );
 }
