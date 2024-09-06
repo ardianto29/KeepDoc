cd69fb45bca5b2066f585db02f45c75b02e2b232
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-06 16:45:35 +0700

    chore(auth): tidy up the code

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
-const SignInPage = () => {
+export function SignInPage() {
   return (
     <main className="auth-page">
       <SignIn />
     </main>
   );
-};
-
-export default SignInPage;
+}
