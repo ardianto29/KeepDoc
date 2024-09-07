7b0f39966c687e73aa30423a9c58c9cf0ee36941
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-08 01:00:41 +0700

    feat: add user id documents

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,24 @@
-export default function Document() {
-  return <div></div>;
+import { CollaborativeRoom } from "@/components";
+import { currentUser } from "@clerk/nextjs/server";
+import { getDocument } from "@/lib/actions/room.actions";
+import { redirect } from "next/navigation";
+
+export default async function Document({ params: { id } }: SearchParamProps) {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  // TODO: Assess the permissions of the user to access the document
+
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+    </main>
+  );
 }
