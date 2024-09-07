ec6de8c2e2f2499437dd96dd8d4d3dc58cde9408
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-08 00:59:54 +0700

    feat: add user sign in Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,48 @@
-import { Button } from "@/components";
+import { AddDocumentBtn, Button, Header } from "@/components";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
-export default function Home() {
+import Icon1 from "@public/assets/icons/doc.svg";
+
+export default async function Home() {
+  const clerkUser = await currentUser();
+  if (!clerkUser) {
+    redirect("/sign-in");
+  }
+
+  const documents = [];
+
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src={Icon1}
+            alt="Document Icon"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   );
 }
