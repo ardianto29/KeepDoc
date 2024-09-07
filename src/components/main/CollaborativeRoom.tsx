3ba0e914e725e59b793c4ee11e9f2442136a6244
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-08 01:00:16 +0700

    feat: add room id

--- src/components/main/CollaborativeRoom.tsx
+++ src/components/main/CollaborativeRoom.tsx
@@ -1,24 +1,30 @@
 "use client";
 
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
-import { Header, Editor } from "@/components";
+import { Header, Editor, ActiveCollaborators } from "@/components";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-export function CollaborativeRoom() {
+export function CollaborativeRoom({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className="collaborative-room">
           <Header>
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="documents-title">This is a fake dokumens</p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
