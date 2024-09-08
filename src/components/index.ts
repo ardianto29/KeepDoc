22e10d6f01e0c9bc515373906644818f99ddd44b
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-09 01:21:42 +0700

    feat(components): add comments

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,7 @@ export * from "./main/Loader";
 export * from "./main/CollaborativeRoom";
 export * from "./main/AddDocumentBtn";
 export * from "./main/ActiveCollaborators";
+export * from "./main/Comments";
 
 //editor
 export * from "./editor/Editor";
