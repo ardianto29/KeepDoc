28b3ad4cf04e431336f0618dbaa8399620f26b7a
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-10 22:03:11 +0700

    feat:(components ui) : add shadcn ui select

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./ui/button";
 export * from "./ui/input";
 export * from "./ui/dialog";
 export * from "./ui/label";
+export * from "./ui/select";
 
 //main
 export * from "./main/Header";
@@ -12,6 +13,8 @@ export * from "./main/AddDocumentBtn";
 export * from "./main/ActiveCollaborators";
 export * from "./main/Comments";
 export * from "./main/ShareModal";
+export * from "./main/UserTypeSelector";
+export * from "./main/Collaborator";
 
 //editor
 export * from "./editor/Editor";
