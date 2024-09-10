fcf4b9424d0f4c6f89f2bbb8ce6c4a9d26bd968b
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-10 22:40:08 +0700

    feat(components ui): add shadcn ui popover

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from "./ui/input";
 export * from "./ui/dialog";
 export * from "./ui/label";
 export * from "./ui/select";
+export * from "./ui/popover";
 
 //main
 export * from "./main/Header";
@@ -16,6 +17,7 @@ export * from "./main/ShareModal";
 export * from "./main/UserTypeSelector";
 export * from "./main/Collaborator";
 export * from "./main/DeleteModal";
+export * from "./main/Notifications";
 
 //editor
 export * from "./editor/Editor";
